60cf080117fa2eb5539b9dbf5158626f32aae4bb
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-08 09:46:24 +0700

    configure hedaer and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <ListEmployeeComponent />
+      <FooterComponent />
     </div>
   );
 }
